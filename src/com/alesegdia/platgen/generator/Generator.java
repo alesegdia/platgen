7175b2fc4233de29d0aa82431a422dee15ef732b
Alejandro Seguí <alesegdia@gmail.com>
2015-08-08 14:04:27 +0200

    adding gaps

--- src/com/alesegdia/platgen/generator/Generator.java
+++ src/com/alesegdia/platgen/generator/Generator.java
@@ -14,25 +14,43 @@ public class Generator {
 	public static final int numZones = 20;
 	public static final Vec2 zoneWidthRange = new Vec2(6,12);
 	public static final int maxDeltaHeight = 10;
+	public static final Vec2 gapWidthRange = new Vec2(3,6);
 	private TileMap tm;
 
 	private List<Rect> blockList = new LinkedList<Rect>();
 	RNG rng = new RNG();
 	
+	int lastX = 0;
+	int lastY = 25;
+	
 	public TileMap Generate() {
 		tm = new TileMap(50,200,TileType.FREE);
-		int lastX = 0;
-		int lastY = 25;
+		boolean lastWasGap = false;
 		for( int i = 0; i < numZones; i++ ) {
-			int width = rng.nextInt(zoneWidthRange.x, zoneWidthRange.y);
-			int height = rng.nextInt(maxDeltaHeight);
-			int sign = (rng.nextFloat() > 0.5 ? 1 : -1);
-			plotFloor(lastX, lastX + width, lastY + height * sign);
-			lastX = lastX + width;
-			lastY = lastY + height * sign;
+			boolean isZone = (rng.nextFloat() > 0.5) || lastWasGap;
+			lastWasGap = !isZone;
+			if( isZone ) {
+				plotZone();
+			} else {
+				plotGap();
+			}
 		}
 		rasterMap();
 		return tm;
+	}
+	
+	private void plotGap() {
+		int width = rng.nextInt(gapWidthRange.x, gapWidthRange.y);
+		lastX += width;
+	}
+
+	private void plotZone() {
+		int width = rng.nextInt(zoneWidthRange.x, zoneWidthRange.y);
+		int height = rng.nextInt(maxDeltaHeight);
+		int sign = (rng.nextFloat() > 0.5 ? 1 : -1);
+		plotFloor(lastX, lastX + width, lastY + height * sign);
+		lastX = lastX + width;
+		lastY = lastY + height * sign;
 	}
 	
 	private void plotFloor(int xFrom, int xTo, int height) {
